82798f7fa97012acb64f2f8ff00565fc3ea922e0
madhavbaikar090-wq <madhavbaikar090@gmail.com>
2026-04-15 08:47:34 +0530

    added products

--- app.py
+++ app.py
@@ -1,130 +1,159 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "lacto_secret"
+app.secret_key = "secret123"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lactocart.db'
-db = SQLAlchemy(app)
+# ---------------- USERS ----------------
+users = {}
 
-# ------------------ DATABASE ------------------
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-
-# ------------------ PRODUCTS ------------------
-
+# ---------------- PRODUCTS ----------------
 products = {
+
     "milk": [
-        {"name": "Amul Gold", "price": 32, "img": "milk.png"},
-        {"name": "Amul Taaza", "price": 28, "img": "milk.png"},
-        {"name": "Mother Dairy", "price": 30, "img": "milk.png"},
+        {"name": "Amul Milk", "price": 30, "img": "milk.png"},
+        {"name": "Mother Dairy Milk", "price": 32, "img": "milk.png"},
         {"name": "Nestle Milk", "price": 34, "img": "milk.png"}
     ],
+
     "curd": [
         {"name": "Amul Curd", "price": 40, "img": "curd.png"},
-        {"name": "Nestle Curd", "price": 45, "img": "curd.png"},
-        {"name": "Mother Dairy Curd", "price": 42, "img": "curd.png"}
+        {"name": "Nestle Curd", "price": 42, "img": "curd.png"}
     ],
+
     "butter": [
         {"name": "Amul Butter", "price": 55, "img": "butter.png"},
         {"name": "Britannia Butter", "price": 60, "img": "butter.png"}
     ],
+
     "cheese": [
         {"name": "Amul Cheese", "price": 65, "img": "cheese.png"},
         {"name": "Go Cheese", "price": 70, "img": "cheese.png"}
-    ]
+    ],
+
+    "paneer": [
+        {"name": "Amul Paneer", "price": 90, "img": "paneer.png"},
+        {"name": "Mother Dairy Paneer", "price": 95, "img": "paneer.png"}
+    ],
+
+    "buttermilk": [
+        {"name": "Amul Buttermilk", "price": 20, "img": "buttermilk.png"},
+        {"name": "Chaas Masala", "price": 25, "img": "buttermilk.png"}
+    ],
+
+    "lassi": [
+        {"name": "Sweet Lassi", "price": 30, "img": "lassi.png"},
+        {"name": "Mango Lassi", "price": 40, "img": "lassi.png"}
+    ],
+
+    "icecream": [
+        {"name": "Vanilla Ice Cream", "price": 60, "img": "icecream.png"},
+        {"name": "Chocolate Ice Cream", "price": 70, "img": "icecream.png"}
+    ],
+
+    "ghee": [
+        {"name": "Amul Ghee", "price": 120, "img": "ghee.png"},
+        {"name": "Patanjali Ghee", "price": 115, "img": "ghee.png"}
+    ],
+
+    "flavoured_milk": [
+    {"name": "Chocolate Milk", "price": 35, "img": "flavoured_milk.png"},
+    {"name": "Strawberry Milk", "price": 38, "img": "flavoured_milk.png"}
+],
+
+"yogurt": [
+    {"name": "Greek Yogurt", "price": 50, "img": "yogurt.png"},
+    {"name": "Flavoured Yogurt", "price": 45, "img": "yogurt.png"}
+],
+
+"cream": [
+    {"name": "Fresh Cream", "price": 60, "img": "cream.png"},
+    {"name": "Whipping Cream", "price": 70, "img": "cream.png"}
+],
+
+"milk_powder": [
+    {"name": "Amul Milk Powder", "price": 120, "img": "milk_powder.png"},
+    {"name": "Nestle Milk Powder", "price": 130, "img": "milk_powder.png"}
+],
+
+"desserts": [
+    {"name": "Rasgulla", "price": 80, "img": "desserts.png"},
+    {"name": "Gulab Jamun", "price": 90, "img": "desserts.png"}
+]
 }
 
-# ------------------ ROUTES ------------------
+# ---------------- ROUTES ----------------
 
 @app.route('/')
 def home():
-    return redirect('/login')
+    return redirect('/register')
 
-# REGISTER
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        user = User(username=request.form['username'],
-                    password=request.form['password'])
-        db.session.add(user)
-        db.session.commit()
+        users[request.form['username']] = request.form['password']
         return redirect('/login')
     return render_template('register.html')
 
-# LOGIN
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(
-            username=request.form['username'],
-            password=request.form['password']
-        ).first()
+        u = request.form['username']
+        p = request.form['password']
 
-        if user:
-            session['user'] = user.username
+        if u in users and users[u] == p:
+            session['user'] = u
             session['cart'] = []
             return redirect('/shop')
 
     return render_template('login.html')
 
-# SHOP
+
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
 
-# PRODUCTS
-@app.route('/products/<category>')
-def products_page(category):
-    return render_template('products.html',
-                           items=products[category],
-                           category=category)
 
-# ADD TO CART
+@app.route('/products/<category>')
+def show_products(category):
+    if category not in products:
+        return redirect('/shop')
+
+    return render_template(
+        'products.html',
+        items=products[category],
+        category=category
+    )
+
+
 @app.route('/add/<name>/<int:price>')
 def add(name, price):
     if 'cart' not in session:
         session['cart'] = []
 
-    session['cart'].append({"name": name, "price": price})
+    session['cart'].append({
+        "name": name,
+        "price": price
+    })
+
     session.modified = True
     return redirect('/cart')
 
-# CART
+
 @app.route('/cart')
 def cart():
     cart = session.get('cart', [])
     total = sum(item['price'] for item in cart)
     return render_template('cart.html', cart=cart, total=total)
 
-# PAYMENT
+
 @app.route('/payment')
 def payment():
     cart = session.get('cart', [])
     total = sum(item['price'] for item in cart)
     return render_template('receipt.html', cart=cart, total=total)
 
-# SEARCH
-@app.route('/search', methods=['POST'])
-def search():
-    query = request.form['search'].lower()
-    result = []
-
-    for cat in products:
-        for item in products[cat]:
-            if query in item['name'].lower():
-                result.append(item)
-
-    return render_template('products.html',
-                           items=result,
-                           category="Search Results")
-
-# ------------------ INIT DB ------------------
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
